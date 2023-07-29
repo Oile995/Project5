01f1033a85f363e4370ae00f408991d8b663a9c8
Oscar Eriksson <oscar.hillbo@gmail.com>
2023-07-29 21:40:52 +0000

    Added checkout app, added crispy_forms Added checkout app to handle checkout of cart Created template, view and from and styled it after code-institutes walkthrough project. Will update to fit this site.

---
+++ checkout/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from .forms import OrderForm
+
+
+def checkout(request):
+    cart = request.session.get('cart', {})
+    if not cart:
+        messages.error(request, "There's nothing in your cart at the moment!")
+        return redirect(reverse('products'))
+
+    order_form = OrderForm()
+    template = 'checkout/checkout.html'
+    context = {
+        'order_form' : order_form,
+    }
+
+    return render(request, template, context)
--- glacial_ac/settings.py
+++ glacial_ac/settings.py
@@ -46,6 +46,10 @@ INSTALLED_APPS = [
     'home',
     'products',
     'cart',
+    'checkout',
+
+    # Other
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -59,6 +63,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'glacial_ac.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -74,8 +80,13 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
                 'cart.contexts.cart_contents',
             ],
+            'builtins' : [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
