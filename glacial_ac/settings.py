2c1bc898c4621385c44d0d983243fce00a89fd61
Oscar Eriksson <oscar.hillbo@gmail.com>
2023-08-04 09:22:34 +0000

    light tweaking for deployment

--- glacial_ac/settings.py
+++ glacial_ac/settings.py
@@ -126,14 +126,14 @@ WSGI_APPLICATION = 'glacial_ac.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 if 'DATABASE_URL' in os.environ:
-    DATABASE = {
-        'default' : dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
 else:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     }
 
