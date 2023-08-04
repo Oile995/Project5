86473b0665c2ab8554ebe3914f04d29e0641f5c8
Oscar Eriksson <oscar.hillbo@gmail.com>
2023-08-04 09:08:08 +0000

    light tweaking for deployment

--- glacial_ac/settings.py
+++ glacial_ac/settings.py
@@ -13,7 +13,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
-
+if os.path.isfile('env.py'):
+     import env
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -214,7 +215,7 @@ STRIPE_CURRENCY = 'usd'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
-DEFAULT_FROM_EMAIL = 'boutiqueado@example.com'
+DEFAULT_FROM_EMAIL = 'glacialac@example.com'
 
 
 
